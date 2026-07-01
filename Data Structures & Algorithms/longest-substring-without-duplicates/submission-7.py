8816618f67cdb746b9e71399f405282f0a8a0628
Tanmay <91007577+Tanmay177@users.noreply.github.com>
2026-07-01 12:51:46 +0530

    Add: longest-substring-without-duplicates - submission-7

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s) #8
+        charSet = set()
+        i = 0
+
+        maxL = 0
+        
+        for j in range(len(s)):
+                while s[j] in charSet:
+                    charSet.discard(s[i])
+                    i += 1
+                charSet.add(s[j])
+                maxL = max(maxL, j-i+1)
+        return maxL
